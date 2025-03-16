cca42f7d607160e26eab648dc9fb9ca84553d406
MEFRREEX <mefrreex@gmail.com>
2025-03-16 23:17:36 +0400

    feat: player health placeholders

--- src/main/kotlin/com/creeperface/nukkitx/placeholders/providers/NukkitProvider.kt
+++ src/main/kotlin/com/creeperface/nukkitx/placeholders/providers/NukkitProvider.kt
@@ -1,9 +1,10 @@
 package com.creeperface.nukkitx.placeholders.providers
 
 import com.creeperface.nukkit.placeholderapi.api.PlaceholderAPI
+import kotlin.math.roundToInt
 
 /**
- * @author CreeperFace
+ * @author CreeperFace, MEFRREEX
  */
 object NukkitProvider {
 
@@ -23,6 +24,22 @@ object NukkitProvider {
 
             visitorLoader {
                 return@visitorLoader player.inventory.itemInHand.id
+            }
+        }
+
+        papi.build<Int>("player_health_rounded_int") {
+            processParameters(true)
+
+            visitorLoader {
+                return@visitorLoader player.health.roundToInt()
+            }
+        }
+
+        papi.build<Float>("player_health_rounded_float") {
+            processParameters(true)
+
+            visitorLoader {
+                return@visitorLoader "%.1f".format(player.health).toFloat()
             }
         }
     }
